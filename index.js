16fb00feba24715abd62f40a92eff2bfddf50e02
hawlan.js <83552463+hawlanjs@users.noreply.github.com>
2021-07-03 00:01:57 +0300

    index.js
    a

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once("ready", () =>{
+    console.log('bot is ready')
+});
+
+
+
+
+client.login("tokni botakay xow");
